d9ac788c0a9c95a23bfeef3b7ec2fee0bf6d6bb0
Haowen Liu <lhw@lunacd.com>
2025-07-14 21:08:56 -0400

    Move into share or local dir

--- src/cimple/common/constants.py
+++ src/cimple/common/constants.py
@@ -2,10 +2,14 @@ import pathlib
 
 cimple_data_dir = pathlib.Path.home() / ".cimple"
 
-cimple_image_dir = cimple_data_dir / "image"
-cimple_extracted_image_dir = cimple_data_dir / "extracted_image"
-cimple_orig_dir = cimple_data_dir / "orig"
-cimple_pkg_build_dir = cimple_data_dir / "pkg_build"
-cimple_pkg_output_dir = cimple_data_dir / "pkg_output"
-cimple_snapshot_dir = cimple_data_dir / "snapshot"
-cimple_pkg_dir = cimple_data_dir / "pkg"
+cimple_share_dir = cimple_data_dir / "share"
+cimple_local_dir = cimple_data_dir / "local"
+
+cimple_image_dir = cimple_share_dir / "image"
+cimple_orig_dir = cimple_share_dir / "orig"
+cimple_snapshot_dir = cimple_share_dir / "snapshot"
+cimple_pkg_dir = cimple_share_dir / "pkg"
+
+cimple_extracted_image_dir = cimple_local_dir / "extracted_image"
+cimple_pkg_build_dir = cimple_local_dir / "pkg_build"
+cimple_pkg_output_dir = cimple_local_dir / "pkg_output"
