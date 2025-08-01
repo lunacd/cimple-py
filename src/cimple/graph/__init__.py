81d5c9e6a013cc5ea93fe6081118c04f5fcc8d32
Haowen Liu <lhw@lunacd.com>
2025-08-01 16:25:34 -0400

    Resolve build-depends in one graph

--- src/cimple/graph/__init__.py
+++ src/cimple/graph/__init__.py
@@ -3,17 +3,60 @@ import networkx as nx
 from cimple import models
 
 
-def get_runtime_dep_graph(snapshot_map: models.snapshot.SnapshotMap) -> nx.DiGraph:
+def get_dep_graph(snapshot_map: models.snapshot.SnapshotMap) -> nx.DiGraph:
     """
-    Constructs a directed graph representing the runtime dependencies of packages.
+    Constructs a directed graph representing the dependencies of packages.
+
+    There are three types of dependencies:
+    1. Source package depends on binary packages (build-depends)
+    2. Binary package depends on other binary packages (depends)
+    3. Binary package depends on the source package that built it (build)
     """
     graph = nx.DiGraph()
 
     for pkg in snapshot_map.pkgs.values():
-        graph.add_node(pkg.name)
+        graph.add_node(pkg.full_name)
 
     for pkg in snapshot_map.pkgs.values():
-        for dep in pkg.depends:
-            graph.add_edge(pkg.name, dep)
+        if models.snapshot.snapshot_pkg_is_src(pkg.root):
+            # Binary packages depends on source package that built them
+            for bin_pkg in pkg.root.binary_packages:
+                dep = f"bin:{bin_pkg}"
+                if not graph.has_node(dep):
+                    raise RuntimeError(
+                        "Corrupted snapshot! "
+                        f"Binary package {bin_pkg} of {pkg.full_name} not found in snapshot."
+                    )
+                graph.add_edge(f"bin:{bin_pkg}", pkg.full_name)
+
+            # Source package build-depends on other source packages
+            for dep in pkg.root.build_depends:
+                dep_name = f"bin:{dep}"
+                if not graph.has_node(dep_name):
+                    raise RuntimeError(
+                        f"Corrupted snapshot! Binary package {dep_name} not found in snapshot. "
+                        f"Required by build-depends of {pkg.full_name}."
+                    )
+                graph.add_edge(pkg.full_name, dep_name)
+
+        elif models.snapshot.snapshot_pkg_is_bin(pkg.root):
+            # Binary package depends on other binary packages
+            for dep in pkg.root.depends:
+                dep_name = f"bin:{dep}"
+                if not graph.has_node(dep_name):
+                    raise RuntimeError(
+                        f"Corrupted snapshot! Binary package {dep_name} not found in snapshot. "
+                        f"Required by depends of {pkg.full_name}."
+                    )
+                graph.add_edge(pkg.full_name, dep_name)
 
     return graph
+
+
+def get_build_depends(dep_graph: nx.DiGraph, src_pkg: models.pkg.SrcPkgId) -> list[str]:
+    """
+    Get all binary packages that are required during the build a source package.
+    """
+    descendents = nx.descendants(dep_graph, f"src:{src_pkg.name}")
+    binary_deps = filter(lambda item: models.pkg.str_pkg_is_bin(item), descendents)
+    return list(binary_deps)
--- src/cimple/models/snapshot.py
+++ src/cimple/models/snapshot.py
@@ -5,16 +5,52 @@ import pydantic
 import cimple.models.pkg as pkg
 
 
-class SnapshotPkg(pydantic.BaseModel):
+class SnapshotSrcPkg(pydantic.BaseModel):
     name: str
     version: str
+    build_depends: list[str] = []
+    binary_packages: list[str]
+    pkg_type: typing.Literal["src"]
+
+    @property
+    def id(self) -> pkg.SrcPkgId:
+        return pkg.SrcPkgId(name=self.name, version=self.version)
+
+
+class SnapshotBinPkg(pydantic.BaseModel):
+    name: str
     sha256: str
     compression_method: typing.Literal["xz"]
     depends: list[str]
-    build_depends: list[str]
+    pkg_type: typing.Literal["bin"]
 
+    @property
+    def id(self) -> pkg.BinPkgId:
+        return pkg.BinPkgId(name=self.name, sha256=self.sha256)
+
+    @property
+    def tarball_name(self) -> str:
+        return f"{self.name}-{self.sha256}.tar.{self.compression_method}"
+
+
+class SnapshotPkg(pydantic.RootModel):
+    root: typing.Union[SnapshotSrcPkg, SnapshotBinPkg] = pydantic.Field(description="pkg_type")  # noqa: UP007
+
+    @property
     def full_name(self) -> str:
-        return f"{self.name}-{self.version}-{self.sha256}.tar.{self.compression_method}"
+        return f"{self.root.pkg_type}:{self.root.name}"
+
+
+def snapshot_pkg_is_src(
+    snapshot_pkg: SnapshotSrcPkg | SnapshotBinPkg,
+) -> typing.TypeGuard[SnapshotSrcPkg]:
+    return pkg.pkg_is_src(snapshot_pkg.id)
+
+
+def snapshot_pkg_is_bin(
+    snapshot_pkg: SnapshotSrcPkg | SnapshotBinPkg,
+) -> typing.TypeGuard[SnapshotBinPkg]:
+    return pkg.pkg_is_bin(snapshot_pkg.id)
 
 
 class SnapshotChanges(pydantic.BaseModel):
@@ -63,7 +99,22 @@ def get_snapshot_map_from_json(snapshot_data: Snapshot) -> SnapshotMap:
     return SnapshotMap(
         version=snapshot_data.version,
         name=snapshot_data.name,
-        pkgs={pkg.name: pkg for pkg in snapshot_data.pkgs},
+        pkgs={pkg.full_name: pkg for pkg in snapshot_data.pkgs},
         ancestor=snapshot_data.ancestor,
         changes=snapshot_data.changes,
     )
+
+
+def get_snapshot_pkg_from_str_id(snapshot_map: SnapshotMap, pkg_str: str) -> SnapshotPkg | None:
+    """
+    Get a package from a snapshot by its string ID.
+    """
+    if pkg_str in snapshot_map.pkgs:
+        return snapshot_map.pkgs[pkg_str]
+    return None
+
+
+def get_snapshot_pkg_from_pkg_id(
+    snapshot_map: SnapshotMap, pkg_id: pkg.PkgId
+) -> SnapshotPkg | None:
+    return get_snapshot_pkg_from_str_id(snapshot_map, pkg_id.root.full_name)
