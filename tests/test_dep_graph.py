624ccd230ba466d05ab98a72e896ac52da141902
Haowen Liu <lhw@lunacd.com>
2025-08-02 13:18:54 -0400

    Fix graph test after refactoring

--- tests/test_dep_graph.py
+++ tests/test_dep_graph.py
@@ -1,13 +1,12 @@
-from cimple import graph, models, snapshot
+from cimple import models, snapshot
 
 
 def test_get_build_depends(basic_cimple_store: None):
     # Given: a basic snapshot
-    snapshot_map = snapshot.ops.load_snapshot("test-snapshot")
-    dep_graph = graph.build_dep_graph(snapshot_map)
+    cimple_snapshot = snapshot.core.load_snapshot("test-snapshot")
 
     # When: getting the build dependencies for a package
-    build_deps = graph.compute_build_depends(dep_graph, models.pkg.SrcPkgId(name="pkg1", version="1.0"))
+    build_deps = cimple_snapshot.build_depends_of(models.pkg.src_pkg_id("pkg1"))
 
     # Then: returns the correct build dependencies, direct and transitive
     assert sorted(build_deps) == ["bin:pkg2-bin", "bin:pkg3-bin"]
