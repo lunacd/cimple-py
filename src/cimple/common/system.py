8079f4744053c0458b911725a40900d56c0ee41a
Haowen Liu <lhw@lunacd.com>
2025-08-03 14:21:33 -0400

    platform_name util

--- src/cimple/common/system.py
+++ src/cimple/common/system.py
@@ -2,6 +2,29 @@ import pathlib
 import platform
 
 
+def platform_name() -> str:
+    """
+    Returns the name of the current platform.
+    """
+    python_system = platform.system()
+    if python_system.startswith("Windows"):
+        os_name = "windows"
+    elif python_system.startswith("Linux"):
+        os_name = "linux"
+    elif python_system.startswith("Darwin"):
+        os_name = "macos"
+    else:
+        raise RuntimeError(f"Unsupported platform: {platform.system()}")
+
+    python_machine = platform.machine()
+    if python_machine == "x86_64":
+        arch = "x86_64"
+    else:
+        raise RuntimeError(f"Unsupported architecture: {python_machine}")
+
+    return f"{os_name}-{arch}"
+
+
 def is_windows() -> bool:
     """
     Check if the current operating system is Windows.
