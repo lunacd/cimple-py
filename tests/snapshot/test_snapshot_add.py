bcf10dc41ddf254c7fef2f3ed1743f2948afe75c
Haowen Liu <lhw@lunacd.com>
2025-09-07 21:37:15 -0400

    Fix Linux test case

--- tests/snapshot/test_snapshot_add.py
+++ tests/snapshot/test_snapshot_add.py
@@ -1,17 +1,20 @@
-from cimple import common, snapshot
+from cimple import common
 from cimple.models import pkg as pkg_models
+from cimple.models import snapshot as snapshot_models
+from cimple.snapshot import core as snapshot_core
+from cimple.snapshot import ops as snapshot_ops
 
 
 def test_snapshot_add(cimple_pi, basic_cimple_store, cygwin_release_content_side_effect, mocker):
     # GIVEN: a root snapshot
     mocker.patch("cimple.pkg.cygwin.requests.get", side_effect=cygwin_release_content_side_effect)
-    root_snapshot = snapshot.core.load_snapshot("root")
+    root_snapshot = snapshot_core.load_snapshot("root")
 
     # WHEN: adding a package to the snapshot
-    new_snapshot = snapshot.ops.add(
+    new_snapshot = snapshot_ops.add(
         root_snapshot,
         [
-            snapshot.ops.VersionedSourcePackage(
+            snapshot_ops.VersionedSourcePackage(
                 name=pkg_models.src_pkg_id("make"), version="4.4.1-2"
             )
         ],
@@ -23,8 +26,8 @@ def test_snapshot_add(cimple_pi, basic_cimple_store, cygwin_release_content_side
     assert pkg_models.src_pkg_id("make") in new_snapshot.pkg_map
     assert pkg_models.bin_pkg_id("make") in new_snapshot.pkg_map
 
-    # TODO: make sure package tarball exists and has the right hash
-    assert (
-        common.constants.cimple_pkg_dir
-        / "make-4.4.1-2-a279dab57d398d93950e9a7cc2855ecdaa05e93519920be87abbd59cb70dee3f.tar.xz"
-    ).exists()
+    # THEN: pkg exists in the pkg store
+    make_bin_pkg = new_snapshot.pkg_map[pkg_models.bin_pkg_id("make")].root
+    assert snapshot_models.snapshot_pkg_is_bin(make_bin_pkg)
+    sha256 = make_bin_pkg.sha256
+    assert (common.constants.cimple_pkg_dir / f"make-4.4.1-2-{sha256}.tar.xz").exists()
