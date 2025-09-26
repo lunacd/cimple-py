62f16284e85e0822889d0d4f1afb62bc60d2c717
Haowen Liu <lhw@lunacd.com>
2025-09-25 21:32:47 -0400

    Fix typing in logging

--- src/cimple/common/logging.py
+++ src/cimple/common/logging.py
@@ -2,24 +2,24 @@ import logging
 import os
 
 logger = logging.getLogger("cimple")
-if "CIMPLE_DEBUG" in os.environ is not None and os.environ["CIMPLE_DEBUG"] != "":
+if "CIMPLE_DEBUG" in os.environ and os.environ["CIMPLE_DEBUG"] != "":
     logger.setLevel(logging.DEBUG)
 else:
     logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
 
-def debug(msg: str, *args):
+def debug(msg: str, *args: object):
     logger.debug(msg, *args)
 
 
-def info(msg: str, *args):
+def info(msg: str, *args: object):
     logger.info(msg, *args)
 
 
-def warning(msg: str, *args):
+def warning(msg: str, *args: object):
     logger.warning(msg, *args)
 
 
-def error(msg: str, *args):
+def error(msg: str, *args: object):
     logger.error(msg, *args)
