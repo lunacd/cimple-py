1478d680638eb09b6cfa7fafc1a5a9cb35cae9f3
Haowen Liu <lhw@lunacd.com>
2025-12-27 23:34:23 -0500

    Add unit test for dumping snapshot

--- tests/snapshot/test_snapshot_core.py
+++ tests/snapshot/test_snapshot_core.py
@@ -1,8 +1,13 @@
 import typing
 
+import cimple.constants
 import cimple.models.pkg
+import cimple.models.snapshot
+import cimple.util
 
 if typing.TYPE_CHECKING:
+    import pyfakefs.fake_filesystem
+
     import tests.conftest
 
 
@@ -28,3 +33,25 @@ def test_add_pkg_to_snapshot(helpers: tests.conftest.Helpers):
     bin_snapshot_pkg = snapshot.bin_pkg_map[bin_pkg_id]
     assert bin_snapshot_pkg.name == "cmake"
     assert source_snapshot_pkg.binary_packages == [bin_pkg_id]
+
+
+def test_snapshot_dump(
+    helpers: tests.conftest.Helpers, fs: pyfakefs.fake_filesystem.FakeFilesystem
+):
+    # GIVEN: a snapshot with a source and binary package
+    cimple.util.ensure_path(cimple.constants.cimple_snapshot_dir)
+    snapshot = helpers.mock_cimple_snapshot([])
+
+    pkg_id = cimple.models.pkg.SrcPkgId("cmake")
+    snapshot.add_src_pkg(pkg_id, "4.0.3-0", [])
+
+    # WHEN: dumping the snapshot
+    snapshot.dump_snapshot()
+
+    # THEN: the snapshot file should exist
+    snapshot_files = list(cimple.constants.cimple_snapshot_dir.iterdir())
+    assert len(snapshot_files) == 1, f"Expected 1 snapshot file, found {len(snapshot_files)}"
+
+    # THEN: snapshot file conforms to snapshot schema
+    with snapshot_files[0].open("r") as f:
+        cimple.models.snapshot.SnapshotModel.model_validate_json(f.read())
