fc4bb57e723bc685fc053956c4ac301f978721a7
Haowen Liu <lhw@lunacd.com>
2026-01-25 20:38:12 -0500

    Fix stream update command

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -180,9 +180,17 @@ class TestStreamCmd:
             ],
             remove=[],
         )
+        expected_bootstrap_changes = cimple.models.snapshot.SnapshotChanges(
+            add=[cimple.models.snapshot.SnapshotChangeAdd(name="bootstrap1", version="1.0")],
+            update=[],
+            remove=[],
+        )
         resolve_changes_mock = mocker.patch(
             "cimple.cmd.stream.cimple.stream.resolve_snapshot_changes",
-            return_value=expected_changes,
+            return_value=cimple.stream.ResolvedSnapshotChanges(
+                pkg_changes=expected_changes,
+                bootstrap_changes=expected_bootstrap_changes,
+            ),
         )
 
         # GIVEN: mocked dump_snapshot to track its invocation
